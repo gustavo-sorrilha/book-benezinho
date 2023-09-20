a7d877e9d91c15f3a28f9b53a9eb971840a23811
rm97068@fiap.com.br <@Peixonauta356>
2023-09-20 20:11:40 -0300

    feat: implementado o Repository da Pessoa Fisica

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -38,20 +38,5 @@ public class Main {
     }
 
 
-    private static Map<String, Object> getProperties() {
-        Map<String, String> env = System.getenv();
-        Map<String, Object> properties = new HashMap<>();
-
-        for (String chave : env.keySet()) {
-            if (chave.contains( "USER_FIAP" )) {
-                properties.put( "jakarta.persistence.jdbc.user", env.get( chave ) );
-            }
-            if (chave.contains( "PASSWORD_FIAP" )) {
-                properties.put( "jakarta.persistence.jdbc.password", env.get( chave ) );
-            }
-            // Outras configurações de propriedade ....
-        }
-        return properties;
-    }
 
 }
---
+++ src/main/java/br/com/fiap/domain/repository/Repository.java
@@ -0,0 +1,33 @@
+package br.com.fiap.domain.repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public interface Repository<T , U> {
+
+    public List<T> findAll();
+
+    public T findById(U id);
+
+    public List<T> findByName(String texto);
+
+    public T persist(T t);
+
+    default Map<String, Object> getProperties() {
+        Map<String, String> env = System.getenv();
+        Map<String, Object> properties = new HashMap<>();
+
+        for (String chave : env.keySet()) {
+            if (chave.contains( "USER_FIAP" )) {
+                properties.put( "jakarta.persistence.jdbc.user", env.get( chave ) );
+            }
+            if (chave.contains( "PASSWORD_FIAP" )) {
+                properties.put( "jakarta.persistence.jdbc.password", env.get( chave ) );
+            }
+            // Outras configurações de propriedade ....
+        }
+        return properties;
+    }
+
+}
